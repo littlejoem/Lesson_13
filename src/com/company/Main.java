7beda195c1fdba6402b8152d1641cd3462cf0fbf
littlejoem <malyshkz79@gmail.com>
2021-06-10 06:24:58 +0200

    init lesson#15

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,9 +6,17 @@ package com.company;
             Person vitya = new Person(120, "Vitya");
             vitya.say("Goerge");
             System.out.println(vitya.name + "'s height is " + vitya.height);
+
             Person vlad = new Person();
             System.out.println(vlad.height);
+
             Person dima = new Person("Dima");
             System.out.println(dima.name + "Dima's height is " + dima.height);
+
+            Student freshman = new Student(76, "Viktor", 2);
+            freshman.tell();
+
+            Student fresh = new Student(280, "Vlad", 4);
+            fresh.tell();
         }
     }
