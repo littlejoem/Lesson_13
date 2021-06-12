e243ebe07a656482a32e8ce274b4d2aff88f43b9
littlejoem <malyshkz79@gmail.com>
2021-06-12 06:52:25 +0200

    init lesson#16

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,20 +3,8 @@ package com.company;
     public class Main {
 
         public static void main(String[] args) {
-            Person vitya = new Person(120, "Vitya");
-            vitya.say("Goerge");
-            System.out.println(vitya.name + "'s height is " + vitya.height);
-
-            Person vlad = new Person();
-            System.out.println(vlad.height);
-
-            Person dima = new Person("Dima");
-            System.out.println(dima.name + "Dima's height is " + dima.height);
-
-            Student freshman = new Student(76, "Viktor", 2);
-            freshman.tell();
-
-            Student fresh = new Student(280, "Vlad", 4);
-            fresh.tell();
+            Shape[] arr = new Shape[] {new Circle(), new Square(), new Triangle()};
+            for (int i = 0; i < arr.length; i++)
+                arr[i].draw();
         }
     }
