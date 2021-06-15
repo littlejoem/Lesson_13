667394534835af59eb1d5729d128bc32906ad747
littlejoem <malyshkz79@gmail.com>
2021-06-15 07:25:23 +0200

    init lesson#18

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,7 @@ package com.company;
     public class Main {
 
         public static void main(String[] args) {
-//            Computer comp = new Computer();
-//            comp.i7.start();
-//            comp.transend.start();
-            new Computer() {
-                void sComp () {
-                    this.i7.start();
-                    this.transend.start();
-                }
-            };
+            Shape circ = new Circle();
+            circ.draw();
         }
     }
---
+++ src/com/company/Shape.java
@@ -0,0 +1,5 @@
+package com.company;
+
+abstract public class Shape {
+    abstract public void draw ();
+}
