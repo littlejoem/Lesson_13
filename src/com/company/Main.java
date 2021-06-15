d81a7932c46adea7182097b72afc141add0bf94f
littlejoem <malyshkz79@gmail.com>
2021-06-15 16:52:23 +0200

    init lesson#21

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,24 @@
 package com.company;
 
     public class Main {
-
-        public static int sum (int a, int b) {
-            return a + b;
-        }
-
-        public static float sum (float a, float b, float c) {
-            return a + b - c;
-        }
-
         public static void main(String[] args) {
-            System.out.println(sum(2, 3));
-            System.out.println(sum(2.5f, 3.89f, 8.456f));
+            Math zx = new Math();
+            final int xx = 99;
+            int x = Math.summ(12, 23);
+            System.out.println(x);
+            System.out.println(zx.mult(12, 23));
+            Math a = new Math();
+            Math aa = new Math();
+            Math aaa = new Math();
+            System.out.println(a.y);
+            System.out.println(aa.yy);
+            System.out.println(aaa.yy);
+            a.y = 24;
+            aa.yy = 26;
+            aaa.yy = 28;
+            System.out.println(a.y);
+            System.out.println(aa.yy);
+            System.out.println(Math.y);
+            System.out.println(aaa.yy);
         }
     }
---
+++ src/com/company/Math.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Math {
+
+    public static int y = 12;
+    public int yy = 13;
+
+    public static int summ (int a, int b) {
+        return a + b;
+    }
+
+    public int mult (int a, int b) {
+        return a * b;
+    }
+}
